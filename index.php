2ab8a8a20b9cc60c1b8ba0c268732cfa7dc79c90
sujan <sujan@192-168-1-108.tpgi.com.au>
2020-10-19 10:07:02 +1100

    project created

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <title>Sujan Bikram Thapa</title>
+    <Body>
+        <h1>Sujan Bikram Thapa</h1>
+        <div>
+            <input type="submit" value="Click Me" onclick="alert"/>
+        </div>
+        <p>My first paragraph</p>
+    </Body>
+</html>
